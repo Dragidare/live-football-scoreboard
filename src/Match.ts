61cb04f9c3146b4deaeb7256bbbefd549672c4b0
Darko Antic <darkoantic@Darkos-MacBook-Pro.fritz.box>
2023-07-17 13:48:44 +0200

    Match class implemented

--- src/Match.ts
+++ src/Match.ts
@@ -1,3 +1,22 @@
 export class Match {
-    
-}
+    private homeTeam: string;
+    private awayTeam: string;
+    private homeScore: number;
+    private awayScore: number;
+  
+    constructor(homeTeam: string, awayTeam: string) {
+      this.homeTeam = homeTeam;
+      this.awayTeam = awayTeam;
+      this.homeScore = 0;
+      this.awayScore = 0;
+    }
+  
+    public updateScore(homeScore: number, awayScore: number): void {
+      this.homeScore = homeScore;
+      this.awayScore = awayScore;
+    }
+  
+    public getScore(): string {
+      return `${this.homeTeam} ${this.homeScore} - ${this.awayScore} ${this.awayTeam}`;
+    }
+  }
